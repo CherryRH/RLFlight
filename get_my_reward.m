44bff12502c20f523700bb097df6151ab4f21a33
Ruihan Chen <crh610721@outlook.com>
2025-05-16 00:27:48 +0800

    feature: 调整参数

--- get_my_reward.m
+++ get_my_reward.m
@@ -10,50 +10,52 @@ function [reward] = get_my_reward(obs, pre_obs, mytime)
     MIN_SPEED = 4;
     % 最大速度
     MAX_SPEED = 10;
+    % 理想角度
+    IDEAL_ANGLE = pi/8;
     % 严重旋转阈值
-    ROTATION_THRESHOLD = pi/2;
+    ROTATION_THRESHOLD = pi/3;
     
     % 距离变化奖励
     current_distance = obs(4);
     prev_distance = pre_obs(4);
     distance_change = prev_distance - current_distance;
     if distance_change > 0
-        distance_change_reward = distance_change * ((MAX_DISTANCE - current_distance) * 0.01 + 1);
+        distance_change_reward = distance_change * ((MAX_DISTANCE - current_distance) * 0.01);
     else
-        distance_change_reward = distance_change * 1;
+        distance_change_reward = distance_change * 10.0;
     end
     
     % 绝对距离奖励
     distance_bonus = 0;
     if current_distance <= IDEAL_DISTANCE
-        distance_bonus = (IDEAL_DISTANCE - current_distance) * 0.1 + 2;
-    elseif current_distance > MAX_DISTANCE
-        distance_bonus = -100;
+        distance_bonus = (IDEAL_DISTANCE - current_distance) * 0.1;
+    elseif current_distance >= MAX_DISTANCE
+        distance_bonus = -300;
     else
-        distance_bonus = -1;
+        distance_bonus = -5;
     end
     
     % 角度对准奖励
     yaw_diff = obs(8);
     pitch_diff = obs(9);
     alignment_reward = 0;
-    if abs(yaw_diff) < pi/8 && abs(pitch_diff) < pi/8
-        alignment_reward = 1;
+    if abs(yaw_diff) < IDEAL_ANGLE && abs(pitch_diff) < IDEAL_ANGLE
+        alignment_reward = 2;
     end
 
     % 旋转惩罚
-    angle_penalty = (abs(yaw_diff) + abs(pitch_diff)) * 0.5;
+    angle_penalty = (abs(yaw_diff) + abs(pitch_diff)) * 1.0;
     rotation_penalty = 0;
     if abs(yaw_diff) > ROTATION_THRESHOLD || abs(pitch_diff) > ROTATION_THRESHOLD
-        rotation_penalty = (abs(yaw_diff) + abs(pitch_diff)) * 1.0;
+        rotation_penalty = (abs(yaw_diff) + abs(pitch_diff)) * 5.0;
     end
     
     % 速度奖励
     speed = norm(obs(10:12));
     if speed > MIN_SPEED
-        speed_reward = min(speed, MAX_SPEED) * 0.5;
+        speed_reward = min(speed, MAX_SPEED) * 0.2;
     else
-        speed_reward = -(MIN_SPEED - speed) * 2.0;
+        speed_reward = -(MIN_SPEED - speed) * 1.0;
     end
     
     % 攻击奖励
